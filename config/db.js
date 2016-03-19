92732e78597c41cdb684b32df87d9051f6a79c44
Axel Niklasson <axel.niklasson@live.com>
2016-03-19 13:25:35 +0100

    Added basic files

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb://<path to your DB>'
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+// Modules
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+// Configs
+var db = require('./config/db');
+
+// Port
+var port = process.env.PORT || 8080; 
+
+// Connect to DB
+mongoose.connect(db.url);
+
+// Setting up Express
+app.use(bodyParser.json()); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override')); 
+app.use(express.static(__dirname + '/public')); 
+
+// Routes
+require('./app/routes')(app);
+
+// Startup
+app.listen(port);    
+
+// Expose app
+module.exports = app;
